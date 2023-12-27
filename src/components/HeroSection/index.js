4980d6ff676c4166fa7aeb48a19f35ffd8701bb2
Varniha <91463219+varniha@users.noreply.github.com>
2023-12-27 15:00:14 +0530

    Update index.js

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/My pic.jpg'
+import HeroImg from '../../images/My pic (2).jpg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
